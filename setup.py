403cb2b6ea9282b74f02ff3f182f972852320c26
Evgeny Tataurov <tatauroff@gmail.com>
2015-11-22 22:46:53 +0500

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='asyncio-apns',
+    version='0.0.1',
+    packages=['asyncio_apns'],
+    url='https://github.com/etataurov/asyncio-apns',
+    license='MIT',
+    author='etataurov',
+    author_email='tatauroff@gmail.com',
+    description='asyncio client for Apple Push Notification Service'
+)
