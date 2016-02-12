b26439d3363886939bb54e9892a9f0defebc81d1
Evgeny Tataurov <tatauroff@gmail.com>
2016-02-12 22:34:34 +0500

    test under py34

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import sys
 from distutils.core import setup
+
+if sys.version_info < (3, 5):
+    install_requires = ["typing"]
+else:
+    install_requires = []
 
 setup(
     name='asyncio-apns',
     version='0.0.1',
+    install_requires=install_requires,
     packages=['asyncio_apns'],
     url='https://github.com/etataurov/asyncio-apns',
     license='MIT',
