8d1b4e1eb1b4107ea3d7aa73db9dedfc2ca9b52e
Evgeny Tataurov <tatauroff@gmail.com>
2016-02-24 23:52:34 +0500

    fixed h2 from github

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 import sys
 from distutils.core import setup
 
+install_requires = ["h2==2.1.3dev"]
 if sys.version_info < (3, 5):
-    install_requires = ["typing"]
-else:
-    install_requires = []
-
+    install_requires.append("typing")
 
 setup(
     name='asyncio-apns',
     version='0.0.1',
     install_requires=install_requires,
-    dependency_links=["git+https://github.com/python-hyper/hyper-h2@2d41ef196c686f0b1895eb0cfa220082cb2c07f1#egg=h2"],
+    dependency_links=["git+https://github.com/python-hyper/hyper-h2@2d41ef196c686f0b1895eb0cfa220082cb2c07f1#egg=h2-2.1.3dev"],
     packages=['asyncio_apns'],
     url='https://github.com/etataurov/asyncio-apns',
     license='MIT',
